aa39f67f0aa24426cd3bf8807d8758d256585c12
DenisKonn <140009429+DenisKonn@users.noreply.github.com>
2023-11-29 07:54:57 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,43 @@
+    for (let i = 0; i < 10; i++) {
+
+function main() {
+
+    const data = [];
+function generateRandomData() {
+        console.log("Random Number: " + item);
+}
+    });
+    return data;
+
+main();
+    }
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+    const data = generateRandomData();
+    data.forEach(item => {if __name__ == "__main__":
+
+    data = generate_random_data()
+    for item in data:
+        print(f"Random Number: {item}")
+
+import random
+def main():
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+
+    main()
+
+    return data
+
+    data = generate_random_data()
+def main():
+
+if __name__ == "__main__":
+    return data
+
+def generate_random_data():
+        print(f"Random Number: {item}")
+    main()
+import random
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
