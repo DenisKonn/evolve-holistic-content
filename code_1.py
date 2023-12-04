6498cf597b928d771655fe1c80816a9028beb051
DenisKonn <140009429+DenisKonn@users.noreply.github.com>
2023-12-04 21:49:25 +0100

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,39 +1,55 @@
-if __name__ == "__main__":
-def main():
+        console.log("Random Number: " + item);
+
+
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+}
+}
+    });
+    }
+    const data = [];
+function generateRandomData() {
+function main() {
+    data.forEach(item => {
+    const data = generateRandomData();
+    return data;
+
+main();import random
 def generate_random_data():
-    main()
-        print(f"Random Number: {item}")
-    data = generate_random_data()
-
-    data = [random.randint(1, 100) for _ in range(10)]
-
-import random
-    return data
     for item in data:
+def main():
+
+if __name__ == "__main__":
+    main()
 
 
-            data[i] = (int) (Math.random() * 100) + 1;
-            System.out.println("Random Number: " + item);
-public class RandomDataGenerator {
+        print(f"Random Number: {item}")
+
+    data = generate_random_data()
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+        for (int i = 0; i < 10; i++) {
     public static void main(String[] args) {
         }
-        int[] data = new int[10];
-    }
-        for (int item : data) {
+            data[i] = (int) (Math.random() * 100) + 1;
+            System.out.println("Random Number: " + item);
         }
 
-        for (int i = 0; i < 10; i++) {
-}if __name__ == "__main__":
-
-        print(f"Random Number: {item}")
-    for item in data:
-    data = [random.randint(1, 100) for _ in range(10)]
-
-def main():
-
+        for (int item : data) {
+}
+        int[] data = new int[10];
+public class RandomDataGenerator {
+    }
     data = generate_random_data()
-import random
     return data
     main()
+    for item in data:
+if __name__ == "__main__":
+
+        print(f"Random Number: {item}")
+
+    data = [random.randint(1, 100) for _ in range(10)]
+import random
 
 def generate_random_data():
+def main():
