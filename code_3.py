0adb72bd87a234dc74cc16b01d99912bcc99480a
DenisKonn <140009429+DenisKonn@users.noreply.github.com>
2023-12-07 16:52:06 +0100

    Update code_3.py

--- code_3.py
+++ code_3.py
@@ -1,43 +1,55 @@
-    for (let i = 0; i < 10; i++) {
-
-function main() {
-
-    const data = [];
-function generateRandomData() {
-        console.log("Random Number: " + item);
-}
-    });
-    return data;
-
-main();
-    }
-        data.push(Math.floor(Math.random() * 100) + 1);
-}
-    const data = generateRandomData();
-    data.forEach(item => {if __name__ == "__main__":
-
-    data = generate_random_data()
     for item in data:
-        print(f"Random Number: {item}")
 
-import random
 def main():
-    data = [random.randint(1, 100) for _ in range(10)]
-def generate_random_data():
+import random
+    data = generate_random_data()
 
     main()
-
-    return data
-
-    data = generate_random_data()
-def main():
-
+    data = [random.randint(1, 100) for _ in range(10)]
 if __name__ == "__main__":
     return data
-
 def generate_random_data():
         print(f"Random Number: {item}")
+
+
     main()
-import random
-    for item in data:
+
+
     data = [random.randint(1, 100) for _ in range(10)]
+def main():
+def generate_random_data():
+if __name__ == "__main__":
+    return data
+    for item in data:
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+import random
+public class RandomDataGenerator {
+        }
+    public static void main(String[] args) {
+            System.out.println("Random Number: " + item);
+    }
+        int[] data = new int[10];
+
+
+        for (int i = 0; i < 10; i++) {
+        for (int item : data) {
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+}function main() {
+        console.log("Random Number: " + item);
+main();
+}
+    data.forEach(item => {
+
+    }
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = generateRandomData();
+
+function generateRandomData() {
+    });
+}
+    for (let i = 0; i < 10; i++) {
+    const data = [];
+    return data;
